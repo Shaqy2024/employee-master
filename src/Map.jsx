14c952f526592a4a3b867143c2d4602e1a23e62c
Shaqy2024 <sakib950006@gmail.com>
2024-07-04 10:48:16 -0700

    first commit

---
+++ src/Map.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <div>
+      
+ 
+      <h1 className='text-3xl'>dshfjsdfhjsdh</h1>
+
+
+
+
+    </div>
+  )
+}
+
+export default Map
